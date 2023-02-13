660fbb760b9a709f06fc1f78935e5d3e6f88f50a
vivek <vivek@gmail.com>
2023-02-13 19:09:26 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+onLoadApi() {
+ this.registrationForm.setValue({
+  userName : 'vivek',
+  passWord : 'vivek',
+  confirmPassword:'vivek',
+  address : (
+    {
+      city:'vivek',
+      state:'vivek',
+      country:'vivek'
+    }
+  )
+ })
+}
+  registrationForm=new FormGroup(
+    {
+      userName : new FormControl(''),
+      passWord : new FormControl(''),
+      confirmPassword:new FormControl(''),
+      address : new FormGroup(
+        {
+          city:new FormControl(''),
+          state:new FormControl(''),
+          country:new FormControl('')
+        }
+      )
+    }
+  )
 }
